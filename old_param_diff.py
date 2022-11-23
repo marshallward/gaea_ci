9978251f1448966010ad439e00d17f5f42fd7120
Marshall Ward <marshall.ward@noaa.gov>
2022-11-23 13:33:07 -0500

    param and regression scripts
    Gather diffs in parameter output.  New version works with git difftool,
    old one does not.  Not sure how/why that happened, but o well.
    
    Also attaching the intel and pgi specific regression test scripts.  No
    idea why they weren't already there...

---
+++ old_param_diff.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+import collections
+import itertools
+import os
+import sys
+
+
+def report_param_diff():
+    # TODO: Validate paths
+    diff_left, diff_right = sys.argv[1:3]
+
+    old_keys = collections.defaultdict(set)
+    new_keys = collections.defaultdict(set)
+
+    param_left = collections.defaultdict(dict)
+    param_right = collections.defaultdict(dict)
+
+    # Generate a global sets of parameters across expts for each parameter file
+    # NOTE: Potentially problematic; consider per-experiment hashing
+    for lwalk, rwalk in zip(os.walk(diff_left), os.walk(diff_right)):
+        lpath, ldirs, lfiles = lwalk
+        rpath, rdirs, rfiles = rwalk
+
+        # Is this always true?  How does git difftool handle add/rm?  TODO
+        assert(lfiles == rfiles)
+
+        for lfile, rfile in zip(lfiles, rfiles):
+            # Skip non-parameter files
+            if not lfile.startswith('MOM_parameter_doc.'):
+                continue
+
+            pfile = lfile.split('.')[1]
+
+            lpar = parse_param(os.path.join(lpath, lfile))
+            rpar = parse_param(os.path.join(rpath, rfile))
+
+            param_left[pfile].update(lpar)
+            param_right[pfile].update(rpar)
+
+            # NOTE: We might not need these anymore, could generate from param?
+            old_keys[pfile].update(param_left[pfile].keys())
+            new_keys[pfile].update(param_right[pfile].keys())
+
+    for pfile in old_keys:
+        added_params = new_keys[pfile] - old_keys[pfile]
+        removed_params = old_keys[pfile] - new_keys[pfile]
+        common_params = old_keys[pfile] & new_keys[pfile]
+
+        if added_params:
+            print("\nAdded to MOM_parameter_doc.{}:".format(pfile))
+            for param in added_params:
+                print("    - {}".format(param))
+
+        if removed_params:
+            print("\nRemoved from MOM_parameter_doc.{}:".format(pfile))
+            for param in removed_params:
+                print("    - {}".format(param))
+
+        if any(
+            param_left[pfile][param]['value'] != param_right[pfile][param]['value']
+            for param in common_params
+        ):
+            print("\nChanged values in MOM_parameter_doc.{}:".format(pfile))
+            for param in common_params:
+                lvalue = param_left[pfile][param]['value']
+                rvalue = param_right[pfile][param]['value']
+                if lvalue != rvalue:
+                    print("    - {}: {} -> {}".format(param, lvalue, rvalue))
+
+        if any(
+            param_left[pfile][param]['desc'] != param_right[pfile][param]['desc']
+            for param in common_params
+        ):
+            print("\nChanged descriptions in MOM_parameter_doc.{}".format(pfile))
+            for param in common_params:
+                ldesc = param_left[pfile][param]['desc']
+                rdesc = param_right[pfile][param]['desc']
+                if ldesc != rdesc:
+                    print('    - {}:'.format(param))
+                    print('<-- |' + '\n    |'.join(ldesc))
+                    print('--> |' + '\n    |'.join(rdesc))
+
+
+def parse_param(path):
+    parameters = {}
+
+    current_module = None
+    current_param = None
+    current_group = None
+
+    with open(path) as param_file:
+        for line_number, rline in enumerate(param_file):
+            line = rline.rstrip('\n')
+
+            # Skip blank lines
+            if not line:
+                continue
+
+            # Update the module status
+            if line.startswith('! ==='):
+                assert line[:13] == '! === module '
+                current_module = line[13:-4]
+                continue
+
+            # Update the current group (if any)
+            # TODO: We don't use this info, will need to do it later!
+            if line.endswith('%'):
+                # TODO: comments? endlines?  what are the rules here?
+                current_group = line.rstrip('%')
+                continue
+
+            # Clear the group on exit
+            if line.startswith('%'):
+                assert(line[1:] == current_group)
+                current_group = None
+                continue
+
+            try:
+                expr, cmt = line.split('!', 1)
+            except ValueError:
+                print(path)
+                print(repr(line))
+                raise
+
+            if expr.rstrip():
+                pname, _, pval = expr.strip().split(None, 2)
+
+                # Build the parameter entry
+                param = {}
+                param['line'] = line_number
+                param['value'] = pval
+                param['desc'] = []
+                param['module'] = current_module
+
+                parameters[pname] = param
+                current_param = param
+
+            if cmt and current_param:
+                current_param['desc'].append(cmt)
+
+    return parameters
+
+
+if __name__ == '__main__':
+    report_param_diff()
